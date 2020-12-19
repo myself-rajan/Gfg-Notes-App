44097b11b0ea29293cc6a6d9881a9cb1ca99708e
Rajan Kumar S <rks9025676@gmail.com>
2020-12-19 14:00:54 +0530

    Created a handleAuth function with sample data.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,6 +7,25 @@ export default class App extends Component {
   state = {
     User: null
   };
+  handleAuth = (username, password) => {
+    const Users = {
+      Rajan: "rks12345",
+      Praveen: "Hello123",
+      Bhushan: "dark456",
+      Rishav: "ris2000",
+      Shivan: "password",
+      Isabel: "coolcat123",
+      Shashi: "akcd@123"
+    };
+    if (!Users[usernsme]) {
+      // User not found
+    } else if (Users[username] && Users[username] !== password) {
+      // Password is wrong
+    } else {
+      // Password is right!
+      this.setState({ User: { Name: username } });
+    }
+  };
   render() {
     return (
       <div className="App">
