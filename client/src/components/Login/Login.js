ed1a61399d85b442dbc17a5854726b836a8ab626
Rajan Kumar S <rks9025676@gmail.com>
2020-12-19 16:15:40 +0530

    Created the Login Form.

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,11 +1,14 @@
 import React from "react";
+import LoginForm from "./_LoginForm";
 
 export const Login = ({ handleAuth }) => {
   return (
     <div>
       <div className="container">
         <div className="row">
-          <div className="col-6">Login</div>
+          <div className="col-6">
+            <LoginForm handleAuth={handleAuth}></LoginForm>
+          </div>
           <div className="col-6">Register</div>
         </div>
       </div>
--- client/src/components/Login/_LoginForm.js
+++ client/src/components/Login/_LoginForm.js
@@ -1,13 +1,50 @@
 import React, { Component } from "react";
+import FormGroup from "../Forms/FormGroup";
 
 class _LoginForm extends Component {
+  state = {
+    Username: "",
+    Password: ""
+  };
+  handleChange = e => {
+    console.log(`${e.target.name}: ${e.target.value}`);
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.handleAuth(this.state.Username, this.state.Password);
+  };
   render() {
     return (
-      <div>
-        <div className="LoginForm">
-          Login Form Here...{typeof this.props.handleAuth}
-        </div>
-      </div>
+      <form className="LoginForm" onSubmit={this.handleSubmit}>
+        {[
+          {
+            Id: "Username",
+            Type: "text",
+            Desc: "Put your username in right case."
+          },
+          {
+            Id: "Password",
+            Type: "password",
+            Desc: "Put your password that you might have forgotten"
+          }
+        ].map((fg, key) => {
+          //fg.Label = fg.Id;
+          //fg.Value = this.state[fg.Id];
+          return (
+            <FormGroup
+              {...fg}
+              Value={this.state[fg.Id]}
+              Label={fg.Id}
+              key={key}
+              onChange={this.handleChange}
+            />
+          );
+        })}
+        <input type="submit" value="Login" className="btn btn-primary" />
+      </form>
     );
   }
 }
