303922c750a27131da07fc23ffbd7e5f70723c08
Rajan Kumar S <rks9025676@gmail.com>
2020-12-19 11:10:48 +0530

    Created two Seperate Login & Welcome components and splited App.js source code in Welcome.js and Login.js

---
+++ client/src/components/Login/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Login = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-6">Login</div>
+          <div className="col-6">Register</div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ client/src/components/Welcome/Welcome.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Welcome = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">Welcome</div>
+        </div>
+      </div>
+    </div>
+  );
+};
