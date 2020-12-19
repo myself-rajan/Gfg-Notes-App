f558fbdbdec30dc2a35c6205efbedf0935fffb0b
Rajan Kumar S <rks9025676@gmail.com>
2020-12-19 13:49:09 +0530

    Passed User value to the Welcome components (using props).

--- client/src/components/Welcome/Welcome.js
+++ client/src/components/Welcome/Welcome.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-export const Welcome = () => {
+export const Welcome = ({ User }) => {
   return (
     <div>
       <div className="container">
         <div className="row">
-          <div className="col-12">Welcome</div>
+          <div className="col-12">Welcome, {User.Name}.</div>
         </div>
       </div>
     </div>
