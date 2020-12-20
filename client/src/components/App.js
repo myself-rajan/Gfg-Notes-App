98618130b44704b9228823d80b605aa5ce917fe2
Rajan Kumar S <rks9025676@gmail.com>
2020-12-20 13:35:20 +0530

    Implemented server side handle Auth.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { AuthUser } from "../services/User";
 import Header from "./Header/Header";
 import { Login } from "./Login/Login";
 import Welcome from "./Welcome/Welcome";
@@ -9,26 +10,39 @@ export default class App extends Component {
     Error: null
   };
   handleAuth = (username, password) => {
-    const Users = {
-      Rajan: "rks12345",
-      Praveen: "Hello123",
-      Bhushan: "dark456",
-      Rishav: "ris2000",
-      Shivan: "password",
-      Isabel: "coolcat123",
-      Shashi: "akcd@123"
-    };
-    if (!Users[username]) {
-      // User not found
-      this.setState({ User: null, Error: "User Not Found!" });
-    } else if (Users[username] && Users[username] !== password) {
-      // Password is wrong
-      this.setState({ User: null, Error: "Wrong Password!" });
-    } else {
-      // Password is right!
-      this.setState({ User: { Name: username }, Error: null });
-    }
+    AuthUser(username, password).then(
+      res =>
+        this.setState({
+          User: res.data.Message,
+          Error: null
+        })
+      // .catch(error => {
+      //   this.setState({
+      //     User: null,
+      //     Error: error.response.data.Message
+      //   });
+      // });
+    );
   };
+  // const Users = {
+  //   Rajan: "rks12345",
+  //   Praveen: "Hello123",
+  //   Bhushan: "dark456",
+  //   Rishav: "ris2000",
+  //   Shivan: "password",
+  //   Isabel: "coolcat123",
+  //   Shashi: "akcd@123"
+  // };
+  // if (!Users[username]) {
+  //   // User not found
+  //   this.setState({ User: null, Error: "User Not Found!" });
+  // } else if (Users[username] && Users[username] !== password) {
+  //   // Password is wrong
+  //   this.setState({ User: null, Error: "Wrong Password!" });
+  // } else {
+  //   // Password is right!
+  //   this.setState({ User: { Name: username }, Error: null });
+  // }
   handleLogout = e => {
     e.preventDefault();
     this.setState({ User: null });
