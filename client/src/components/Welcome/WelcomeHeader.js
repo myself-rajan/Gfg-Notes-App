f0ef44b92524037294e1cabed00d10bd30e88bc6
Rajan Kumar S <rks9025676@gmail.com>
2020-12-20 09:24:40 +0530

    Added link to home page and bug fixes.

--- client/src/components/Welcome/WelcomeHeader.js
+++ client/src/components/Welcome/WelcomeHeader.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const WelcomeHeader = ({ User, handleLogout }) => {
   return (
     <div className="row">
       <div className="col-12">
         <div className="border rounded p-2">
-          <p>Welcome, {User.Name}.</p>
+          <p className="mb-1">Welcome, {User.Name}.</p>
+          <Link to="/" className="btn btn-primary btn-sm mr-2">
+            Home
+          </Link>
           <button className="btn btn-danger btn-sm" onClick={handleLogout}>
             Logout
           </button>
