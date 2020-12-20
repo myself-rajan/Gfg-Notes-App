b6c573193f1daf1f904d2884a98bce4d96f910ba
Rajan Kumar S <rks9025676@gmail.com>
2020-12-20 17:08:10 +0530

    Implemented API creation of a new note.

--- server/routes/note.js
+++ server/routes/note.js
@@ -37,4 +37,23 @@ note.get("/", (req, res) => {
   res.json(Notes);
 });
 
+note.post("/", (req, res) => {
+  const { NoteId, Title, DESC, User } = req.body;
+  if (Notes.find(n => n.NoteID === NoteId)) {
+    re.status(409).json({
+      Error: true,
+      Success: false,
+      Message: "Note ID aleady taken!"
+    });
+  } else {
+    Notes.push({ NoteId, Title, DESC, User });
+    res.status(201).json({
+      Error: false,
+      Success: true,
+      Message: `Note ${Title} created successfully`
+    });
+  }
+  res.json({ NoteId, Title, DESC, User });
+});
+
 module.exports = note;
