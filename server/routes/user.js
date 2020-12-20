19628b3112850bfcf8df28b5c9b8c1f163e9a5b9
Rajan Kumar S <rks9025676@gmail.com>
2020-12-20 11:05:57 +0530

    Created user route with our sample users.

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,49 @@
-const app = require("express").Router();
+const user = require("express").Router();
 
-app.get("/", (req, res) => {
-  res.json("Welcome to /api/user!");
+//Dummy list of users for now.
+const Users = {
+  Rajan: "rks12345",
+  Praveen: "Hello123",
+  Bhushan: "dark456",
+  Rishav: "ris2000",
+  Shivan: "password",
+  Isabel: "coolcat123",
+  Shashi: "akcd@123"
+};
+
+user.get("/", (req, res) => {
+  res.json(req.session.User);
+});
+user.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (!Users[username]) {
+    // User not found
+    req.session.destroy();
+    res.status(404).json({
+      Error: true,
+      Success: false,
+      Message: "User not Found"
+    });
+  } else if (Users[username] && Users[username] !== password) {
+    // Password is wrong
+    //this.setState({ User: null, Error: "Wrong Password!" });
+    req.session.destroy();
+    res.status(403).json({
+      Error: true,
+      Success: false,
+      Message: "Invalid username and password!"
+    });
+  } else {
+    // Password is right!
+    //this.setState({ User: { Name: username }, Error: null });
+    res.json({
+      Error: false,
+      Success: true,
+      Message: {
+        Name: username
+      }
+    });
+  }
 });
 
 module.exports = app;
