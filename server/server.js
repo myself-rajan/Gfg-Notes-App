7404cab190a58f2120008f0a32c7977ba6263577
Rajan Kumar S <rks9025676@gmail.com>
2020-12-20 10:52:14 +0530

    Implemented express session in server(server.js).

--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const morgan = require("morgan");
+const session = require("express-session");
 const app = express();
 const api = require("./routes/api");
 const port = 3100;
 
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+  })
+);
 
 app.use("/api", api); // This is the Route handler
 app.get("/", (req, res) => {
